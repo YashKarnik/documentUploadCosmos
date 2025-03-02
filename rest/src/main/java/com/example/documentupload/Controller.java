c0de7ddac7694376879f0cf09184d53bf4d33c55
Yash Karnik <yashkarnik2000@gmail.com>
2025-03-03 01:39:22 +0530

    add env var

--- rest/src/main/java/com/example/documentupload/Controller.java
+++ rest/src/main/java/com/example/documentupload/Controller.java
@@ -94,7 +94,7 @@ public class Controller {
     public Map<String, String> getEnvVars() {
         Map<String, String> env = System.getenv();
         for (String envName : env.keySet()) {
-            System.out.format("%s=%s%n", envName, env.get(envName));
+            System.out.format("%s==>%s%n", envName, env.get(envName));
         }
         return env
     }
